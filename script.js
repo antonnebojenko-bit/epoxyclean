554960ac1997f1399c47b6c158c953816f60cac2
antonnebojenko-bit <antonnebojenko@gmail.com>
2026-07-22 18:37:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+document.addEventListener("DOMContentLoaded", function(){
 
+const images = document.querySelectorAll(".gallery img");
+
+
+images.forEach(function(image){
+
+image.addEventListener("click", function(){
+
+const overlay = document.createElement("div");
+
+overlay.className = "image-overlay";
+
+
+overlay.innerHTML = `
+<img src="${image.src}">
+`;
+
+
+document.body.appendChild(overlay);
+
+
+overlay.addEventListener("click", function(){
+
+overlay.remove();
+
+});
+
+});
+
+});
+
+
+});
